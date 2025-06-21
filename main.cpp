2b417c68460d57b721028997a406a48d5fdd7352
谢延盛 2023051604018 <463311564@qq.com>
2025-06-21 23:54:53 +0800

    SHX_Music v0.7.6:add QImage deal

--- main.cpp
+++ main.cpp
@@ -1,35 +1,43 @@
-#include <QApplication>
+#include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include "./heads/musicpathoperations.h"
 #include "./heads/musicinfo.h"
 #include "./heads/musicmodel.h"
 #include "./heads/fileOperations.h"
+#include "./heads/imageproviader.h"
 
 int main(
     int argc, char *argv[])
 {
     qputenv("QT_QPA_PLATFORMTHEME", "xdgdesktopportal");
-    QApplication app(argc, argv);
+
+    QGuiApplication app(argc, argv);
 
     MusicInfo musicInfo;
     MusicPathOperations mpo;
     FileOperations fileOperaTions;
+    ImageProviader imageProviader;
+
+    // QImage image("path/to/your/image.jpg");
+    // imageProvider.setImage(image);
 
     qmlRegisterType<MusicModel>("MyModel", 1, 0, "MusicModel");
-    //qmlRegisterType<MusicModel>("MyModel", 1, 0, "FileOperations");
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("MusicInfo", &musicInfo);
     engine.rootContext()->setContextProperty("MusicPathOperations", &mpo);
-    //engine.rootContext()->setContextProperty("FileOperaTions", &fileOperaTions);
+    engine.rootContext()->setContextProperty("FileOperaTions", &fileOperaTions);
+    engine.rootContext()->setContextProperty("ImageProviader", &imageProviader);
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+                     Qt::QueuedConnection);
     engine.loadFromModule("SHXM", "Main");
 
     return app.exec();
 }
+
